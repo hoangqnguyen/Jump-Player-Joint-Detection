73bd07c4a51d0a61a1612ae7a9c81a735478442b
hoang <hoang@r48p.kist>
2026-01-14 12:44:24 +0900

    Increase training epochs to 100 and batch size to 8 for improved model performance

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ model = RFDETRMedium()
 
 model.train(
     dataset_dir="data/VNL_500Videos_RTDETR",
-    epochs=10,
-    batch_size=4,
+    epochs=100,
+    batch_size=8,
     grad_accum_steps=4,
     lr=1e-4,
     output_dir="./finetune/",
